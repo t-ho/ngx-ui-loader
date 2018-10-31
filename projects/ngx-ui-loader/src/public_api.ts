900e63120ac1ceb2c0665f58afda7876cbf10a1b
t-ho <toan.hmt@gmail.com>
2018-10-31 22:55:08 +0700

    Remove NGX_POSITIONS, PB_DIRECTIONS, SPINNER_TYPES

--- projects/ngx-ui-loader/src/public_api.ts
+++ projects/ngx-ui-loader/src/public_api.ts
@@ -5,7 +5,7 @@
 export { NgxUiLoaderService } from './lib/core/ngx-ui-loader.service';
 export { NgxUiLoaderModule } from './lib/core/ngx-ui-loader.module';
 export { NgxUiLoaderConfig } from './lib/core/ngx-ui-loader-config';
-export { SPINNER, SPINNER_TYPES, NGX_POSITIONS, POSITION, PB_DIRECTION, PB_DIRECTIONS } from './lib/core/ngx-ui-loader.enums';
+export { SPINNER, POSITION, PB_DIRECTION } from './lib/core/ngx-ui-loader.enums';
 export { NgxUiLoaderRouterModule } from './lib/router/ngx-ui-loader-router.module';
 export { NgxUiLoaderRouterConfig } from './lib/router/ngx-ui-loader-router-config';
 export { NgxUiLoaderHttpModule } from './lib/http/ngx-ui-loader-http.module';
