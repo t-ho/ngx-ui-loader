04ac6e22d5c6a4f97ddf487ea868d9e8beb01fb6
t-ho <toan.hmt@protonmail.com>
2019-05-18 13:54:04 -0700

    feat(schematics): Add basic ng-add and ng-update

---
+++ projects/ngx-ui-loader/schematics/update/index.ts
@@ -0,0 +1,7 @@
+import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+
+export function update722(_options: any): Rule {
+  return (tree: Tree, _context: SchematicContext) => {
+    return tree;
+  }
+}
