dfe761cfd5e1bfddf1277582d037e257d3f8876f
t-ho <t.ho@tdev.app>
2021-04-27 20:17:50 -0700

    Update schematics

--- projects/ngx-ui-loader/schematics/update/index.ts
+++ projects/ngx-ui-loader/schematics/update/index.ts
@@ -4,3 +4,7 @@ import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 export function update1000(_options: any): Rule {
   return (tree: Tree, _context: SchematicContext) => tree;
 }
+
+export function update1100(_options: any): Rule {
+  return (tree: Tree, _context: SchematicContext) => tree;
+}
